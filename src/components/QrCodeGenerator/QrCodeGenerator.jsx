2eff12a088b8b2baaa48e72499a8c160f541ffa9
Oleksandr Dzisiak <shumahersasha007@gmail.com>
2023-03-06 00:09:34 +0100

    add styles

--- src/components/QrCodeGenerator/QrCodeGenerator.jsx
+++ src/components/QrCodeGenerator/QrCodeGenerator.jsx
@@ -28,10 +28,10 @@ function QrCodeApp() {
   let btn =
     "px-4 py-2 font-medium text-xl uppercase text-slate-100 bg-slate-700 rounded-md w-80 hover:bg-slate-200 hover:text-slate-700 hover:-translate-y-1 transition-all duration-300";
   return (
-    <div className="container mx-auto h-screen">
+    <div className="container mx-auto">
       <div className="">
-        <div className="flex flex-col items-center gap-3">
-          <h1 className="font-bold text-5xl text-center mb-5 text-slate-100">
+        <div className="flex flex-col items-center gap-3 w-8">
+          <h1 className="font-bold text-3xl md:text-5xl text-center mb-5 text-slate-100 px-2 ">
             QR-Code vCard 3.0 Generator
           </h1>
           <input
