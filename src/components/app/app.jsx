246dd1bbefcf6fd20cf60d02ca31f9ac7998f52d
Oleksandr Dzisiak <shumahersasha007@gmail.com>
2023-03-06 00:26:24 +0100

    change style

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -2,8 +2,8 @@ import QrCodeApp from "../QrCodeGenerator/QrCodeGenerator";
 
 function App() {
   return (
-    <div className=" bg-gray-100 dark:bg-slate-900 dark:text-slate-100 text-slate-900 ">
-      <div className="container mx-auto grid place-items-center h-screen py-5">
+    <div className=" py-5 bg-gray-100 dark:bg-slate-900 dark:text-slate-100 text-slate-900 grid place-items-center min-h-screen">
+      <div className="w-full mx-auto grid place-items-center">
         <QrCodeApp />
       </div>
     </div>
